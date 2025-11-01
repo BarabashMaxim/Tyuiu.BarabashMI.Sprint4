fcfaab39c837d300ad185035e2fb9d150cfd61a5
BarabashMaxim <Tyuiu.BarabashMI@outlook.com>
2025-11-01 12:04:06 +0500

    Task2

---
+++ Tyuiu.BarabashMI.Sprint4.Task2.V26.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.BarabashMI.Sprint4.Task2.V26.Lib;
+namespace Tyuiu.BarabashMI.Sprint4.Task2.V26.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int[] nums = new int[14] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
+            int wait = 1;
+            int res = ds.Calculate(nums);
+            Assert.AreEqual(wait, res);
+
+        }
+    }
+}
---
+++ Tyuiu.BarabashMI.Sprint4.Task2.V26/Program.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.BarabashMI.Sprint4.Task2.V26.Lib;
+class Program
+{
+    static void Main(string[] args)
+    { 
+        DataService ds = new DataService();
+        int p = 1;
+        int[] nums = new int[12];
+        for (int i = 0; i < nums.Length; i++)
+        { 
+            string input = Console.ReadLine();
+            nums[i] = Convert.ToInt32(input);
+        }
+        Console.WriteLine(ds.Calculate(nums));2
+    }
+}
