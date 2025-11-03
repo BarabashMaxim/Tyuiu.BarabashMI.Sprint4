8bcc79507a77ab2994d4d117088c48a657744815
BarabashMaxim <Tyuiu.BarabashMI@outlook.com>
2025-11-03 12:50:44 +0500

    Task5

---
+++ Tyuiu.BarabashMI.Sprint4.Task5.V3.Test/DataServiceTest.cs
@@ -0,0 +1,38 @@
+﻿using Tyuiu.BarabashMI.Sprint4.Task5.V3.Lib;
+namespace Tyuiu.BarabashMI.Sprint4.Task5.V3.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int[,] matr =
+            {
+                {-1,1,-1,1,1 },
+                {1,-1,-1,1,-1 },
+                {1,1,1,-1,-1 },
+                {-1,1,1,-1,-1 },
+                {1,-1,-1,-1,1 }
+            };
+            int[,] wait =
+            {
+                {0,1,0,1,1 },
+                {1,0,0,1,0 },
+                {1,1,1,0,0 },
+                {0,1,1,0,0 },
+                {1,0,0,0,1 }
+            };
+            int[,] res = ds.Calculate(matr);
+            bool m = true;
+            for (int i = 0; i < res.GetLength(0); i++)
+                for (int j = 0; j < res.GetLength(1); j++)
+                    if (wait[i,j] != res[i,j])
+                        m = false;
+            bool t = true;
+            
+            Assert.AreEqual(t,m);
+        }
+    }
+}
---
+++ Tyuiu.BarabashMI.Sprint4.Task5.V3/Program.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.BarabashMI.Sprint4.Task5.V3.Lib;
+class Program
+{
+    static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        int n, m;
+        n = Convert.ToInt32(Console.ReadLine());
+        m = Convert.ToInt32(Console.ReadLine());
+        int[,] matr = new int[n, m];
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                matr[i, j] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+        matr = ds.Calculate(matr);
+        
+    }
+}
