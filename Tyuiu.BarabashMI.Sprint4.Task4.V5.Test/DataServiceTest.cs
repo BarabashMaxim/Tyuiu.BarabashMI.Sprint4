0414fa5ce40eb3d7382931b1662f1c4151ed07a3
BarabashMaxim <Tyuiu.BarabashMI@outlook.com>
2025-11-03 12:24:24 +0500

    Task4

---
+++ Tyuiu.BarabashMI.Sprint4.Task4.V5.Test/DataServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using Tyuiu.BarabashMI.Sprint4.Task4.V5.Lib;
+namespace Tyuiu.BarabashMI.Sprint4.Task4.V5.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        { 
+            DataService ds = new DataService();
+            int[,] matr =
+            {
+                {6,3,6,6,7 },
+                {9,8,9,7,9 },
+                {5,6,8,7,5 },
+                {8,5,6,5,3 },
+                {3,9,9,8,8 }
+            };
+            int wait = 70;
+            int res = ds.Calculate(matr);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.BarabashMI.Sprint4.Task4.V5/Program.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.BarabashMI.Sprint4.Task4.V5.Lib;
+class Program
+{
+    static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        int n,m;
+        n = Convert.ToInt32(Console.ReadLine());
+        m = Convert.ToInt32(Console.ReadLine());
+        int[,] matr = new int[n,m];
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                matr[i,j] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+        Console.WriteLine(ds.Calculate(matr));
+    }
+}
